ed74c0789f38f89d57d144095720dd51188c8431
Kryvko97 <108485653+Kryvko97@users.noreply.github.com>
2023-07-21 10:45:22 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// Додаємо бібліотеку Vimeo
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+persistedCurrentTime ();
+
+player.on('timeupdate', throttle(timeupdateVideo, 1000));
+
+// Зберігаємо час відтворення відео в локальному сховищі
+function timeupdateVideo (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+// Збереження часу відтворення при перезавантаженні сторінки
+function persistedCurrentTime () {
+    let currentTime = localStorage.getItem('videoplayer-current-time');
+
+    if (currentTime) {
+        player.setCurrentTime(currentTime).then(function(currentTime) {
+           return currentTime;
+        })
+    }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
 
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+form.addEventListener('submit', onFormSubmit)
+form.addEventListener('input', throttle(onFormInput, 500))
+
+populateMessageOutput();
+
+// Зберігаємо дані форми в локальному сховищі та змінній formData
+function onFormInput(e) {
+formData[e.target.name] = e.target.value;
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+// Подія при кліку на кнопку
+function onFormSubmit(e) {
+e.preventDefault();
+
+if (form.email.value === "" || form.message.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+e.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+console.log(formData);
+}
+
+// Повертаємо дані форми при перезавантаженні сторінки
+function populateMessageOutput() {
+let persistedForm = localStorage.getItem(STORAGE_KEY)
+
+if (persistedForm) {
+    persistedForm = JSON.parse(persistedForm);
+    Object.entries(persistedForm).forEach(([email, message]) => {
+form.elements[email].value = message;
+formData[email] = message;
+    })
+}
+}
